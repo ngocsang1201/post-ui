9cbb2ed1e1b627b4d019f677fd153dec3c0447f5
Sangg <ngocsang1201@gmail.com>
2022-01-15 15:45:05 +0700

    Get form values on submit

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,11 +1,30 @@
 import { setFieldValue, setImage } from './common'
 
+const fieldNames = ['title', 'author', 'description', 'imageUrl']
+
 const setFormValues = (form, values) => {
-  setFieldValue(form, '[name="title"]', values.title)
-  setFieldValue(form, '[name="author"]', values.author)
-  setFieldValue(form, '[name="description"]', values.description)
-  setFieldValue(form, '[name="imageUrl"]', values.imageUrl)
+  for (const name of fieldNames) {
+    setFieldValue(form, `[name='${name}']`, values?.[name])
+  }
   setImage(document, '#postHeroImage', values.imageUrl)
+}
+
+const getFormValues = (form) => {
+  const formValues = {}
+
+  // S1: get value each input field
+  // for (const name of fieldNames) {
+  //   const field = form.querySelector(`[name='${name}']`)
+  //   formValues[name] = field.value
+  // }
+
+  // S2: using form data
+  const formData = new FormData(form)
+  for (const [field, value] of formData) {
+    formValues[field] = value
+  }
+
+  return formValues
 }
 
 export const initPostForm = ({ formId, defaultValues, onSubmit }) => {
@@ -13,4 +32,11 @@ export const initPostForm = ({ formId, defaultValues, onSubmit }) => {
   if (!form) return
 
   setFormValues(form, defaultValues)
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const formValues = getFormValues(form)
+    console.log(formValues)
+  })
 }
