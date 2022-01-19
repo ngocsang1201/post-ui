1d99bde31648ff4029c17ace15fe9263ccd38e60
Sangg <ngocsang1201@gmail.com>
2022-01-19 20:53:53 +0700

    Submit form-data

--- js/add-edit-post.js
+++ js/add-edit-post.js
@@ -12,24 +12,35 @@ const removeUnusedFields = (formValues) => {
 
   delete payload.imageSource
 
+  if (!payload.id) delete payload.id
+
   return payload
 }
 
-const handleFormSubmit = async (formValues) => {
-  const payload = removeUnusedFields(formValues)
-  console.log('Submit form', { formValues, payload })
-  return
+const objectToFormData = (obj) => {
+  const formData = new FormData()
 
+  for (const key in obj) {
+    formData.set(key, obj[key])
+  }
+
+  return formData
+}
+
+const handleFormSubmit = async (formValues) => {
   try {
+    const payload = removeUnusedFields(formValues)
+    const formData = objectToFormData(payload)
+
     const savedPost = formValues.id
-      ? await postApi.update(formValues)
-      : await postApi.add(formValues)
+      ? await postApi.updateFormData(formData)
+      : await postApi.addFormData(formData)
 
     toast.success('Saved post successfully!')
 
-    setTimeout(() => {
-      window.location.assign(`/post-detail.html?id=${savedPost.id}`)
-    }, 2000)
+    // setTimeout(() => {
+    //   window.location.assign(`/post-detail.html?id=${savedPost.id}`)
+    // }, 2000)
   } catch (error) {
     console.log('Failed to submit post form', error)
     toast.error(error.message)
--- js/api/postApi.js
+++ js/api/postApi.js
@@ -21,6 +21,24 @@ const postApi = {
     return axiosClient.patch(url, data)
   },
 
+  addFormData(formData) {
+    const url = '/with-thumbnail/posts'
+    return axiosClient.post(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
+
+  updateFormData(formData) {
+    const url = `/with-thumbnail/posts/${formData.get('id')}`
+    return axiosClient.patch(url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
+
   remove(id) {
     const url = `/posts/${id}`
     return axiosClient.delete(url)
